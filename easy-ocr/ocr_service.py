2b2d73a3f6a54e0dd59c687c102cad2373c9ab82
tomorrowknight <alvinarulselvan.com>
2024-12-27 23:07:47 +0800

    moved to pytesseract

--- easy-ocr/ocr_service.py
+++ easy-ocr/ocr_service.py
@@ -1,19 +1,32 @@
+import os
 from flask import Flask, request, jsonify
-import easyocr
+from PIL import Image
+import pytesseract
 
 app = Flask(__name__)
 
-@app.route('/ocr', methods=['POST'])
-def ocr():
+@app.route("/")
+def home():
+    return "OCR Service is Running!"
+
+@app.route("/ocr", methods=["POST"])
+def process_ocr():
     if 'file' not in request.files:
-        return jsonify({'error': 'No file uploaded'}), 400
-
+        return jsonify({"error": "No file uploaded"}), 400
+    
     file = request.files['file']
-    reader = easyocr.Reader(['en'], gpu=False) 
-    results = reader.readtext(file.read())
-    extracted_text = ' '.join([res[1] for res in results])
 
-    return jsonify({'text': extracted_text})
+    try:
+        # Open the image file
+        image = Image.open(file.stream)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5550)
+        # Perform OCR
+        text = pytesseract.image_to_string(image)
+
+        return jsonify({"text": text}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
